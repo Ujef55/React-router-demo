24eaa3e57bdccf7cc266e4262ab619f919ed0228
Ujef <ujef.kelawala123@gmail.com>
2024-09-22 17:06:14 +0530

    add few components and basic router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Movies from './components/Movies';
+import Contact from './components/Contact';
 
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Home />
+    },
+    {
+      path: '/about',
+      element: <About />
+    },
+    {
+      path: '/movies',
+      element: <Movies />
+    },
+    {
+      path: 'montact',
+      element: <Contact />
+    }
+  ]);
+
+
   return (
-    <h1>React-router.dom</h1>
+    <RouterProvider router={router} />
   )
 }
 
---
+++ src/components/Movies.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Movies() {
+    return (
+        <h1>This is movie cards page</h1>
+    )
+}
+
+export default Movies;
